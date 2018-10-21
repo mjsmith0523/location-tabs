ab375561b4c908a6a68e5226a0ef578c71665948
Matthew Smith <matthewjsmith0523@gmail.com>
2018-10-21 16:44:28 -0500

    Update locations-tabs.php

---
+++ locations-tabs.php
@@ -0,0 +1,139 @@
+<div class="tab-container-locations" id="tabs-container">
+    <div class="wrap--inner">
+        <div class="tabs-nav">
+            
+            <div class="wrap__cta__tab-location1 tablinks active tab" data-tab="tab__location1">
+                {% component 'cta_location1' %}
+            </div>
+            <div class="tab-wrapper wrap__tab-location1" id="tab__location1">
+                <div class="tabs-panel-content">
+                    <div class="wrap__grid_cols">
+                        <div class="wrap__grid_cols--6">
+                            <div class="wrap__map">
+                                <div class="wrap__map--inner">
+                                    {% component "editable" file="map-location-1.htm" %}
+                                </div>
+                            </div>
+                        </div>
+                        <div class="wrap__grid_cols--6">
+                            <div class="wrap__hours">
+                                <div class="wrap__hours--inner">
+                                    {% component 'hours_location_1' %}
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div style="clear:both"></div>
+                </div>
+            </div>
+            
+            <div class="wrap__cta__tab-location2 tablinks tab" data-tab="tab__location2">
+                {% component 'cta_location2' %}
+            </div>
+            <div class="tab-wrapper wrap__tab-location2 hide" id="tab__location2">
+                <div class="tabs-panel-content">
+                    <div class="wrap__grid_cols">
+                        <div class="wrap__grid_cols--6">
+                            <div class="wrap__map">
+                                <div class="wrap__map--inner">
+                                    {% component "editable" file="map-location-2.htm" %}
+                                </div>
+                            </div>
+                        </div>
+                        <div class="wrap__grid_cols--6">
+                            <div class="wrap__hours">
+                                <div class="wrap__hours--inner">
+                                    {% component 'hours_location_2' %}
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div style="clear:both"></div>
+                </div>
+            </div>
+
+            <div class="wrap__cta__tab-location3 tablinks tab" data-tab="tab__location3">
+                {% component 'cta_location3' %}
+            </div>
+            <div class="tab-wrapper wrap__tab-location3 hide" id="tab__location3">
+                <div class="tabs-panel-content">
+                    <div class="wrap__grid_cols">
+                        <div class="wrap__grid_cols--6">
+                            <div class="wrap__map">
+                                <div class="wrap__map--inner">
+                                    {% component "editable" file="map-location-3.htm" %}
+                                </div>
+                            </div>
+                        </div>
+                        <div class="wrap__grid_cols--6">
+                            <div class="wrap__hours">
+                                <div class="wrap__hours--inner">
+                                    {% component 'hours_location_3' %}
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div style="clear:both"></div>
+                </div>
+            </div>
+
+            <div class="wrap__cta__tab-location4 tablinks tab" data-tab="tab__location4">
+                {% component 'cta_location4' %}
+            </div>
+            <div class="tab-wrapper wrap__tab-location4 hide" id="tab__location4">
+                <div class="tabs-panel-content">
+                    <div class="wrap__grid_cols">
+                        <div class="wrap__grid_cols--6">
+                            <div class="wrap__map">
+                                <div class="wrap__map--inner">
+                                    {% component "editable" file="map-location-4.htm" %}
+                                </div>
+                            </div>
+                        </div>
+                        <div class="wrap__grid_cols--6">
+                            <div class="wrap__hours">
+                                <div class="wrap__hours--inner">
+                                    {% component 'hours_location_4' %}
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div style="clear:both"></div>
+                </div>
+            </div>
+
+            <div class="wrap__cta__tab-location5 tablinks tab" data-tab="tab__location5">
+                {% component 'cta_location5' %}
+            </div>
+            <div class="tab-wrapper wrap__tab-location5 hide" id="tab__location5">
+                <div class="tabs-panel-content">
+                    <div class="wrap__grid_cols">
+                        <div class="wrap__grid_cols--6">
+                            <div class="wrap__map">
+                                <div class="wrap__map--inner">
+                                    {% component "editable" file="map-location-5.htm" %}
+                                </div>
+                            </div>
+                        </div>
+                        <div class="wrap__grid_cols--6">
+                            <div class="wrap__hours">
+                                <div class="wrap__hours--inner">
+                                    {% component 'hours_location_5' %}
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div style="clear:both"></div>
+                </div>
+            </div>
+
+            
+            
+            
+            
+            
+        </div>
+        <div class="tabs-list"></div>
+    </div>
+</div>
+ <div style="clear:both"></div>
